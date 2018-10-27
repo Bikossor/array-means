2324f01b3c227afd51fe4694a958cac454afc213
Dergeraetwirdniemuede <44534540+Dergeraetwirdniemuede@users.noreply.github.com>
2018-10-27 23:53:50 +0200

    Closes #2

---
+++ index.js
@@ -0,0 +1,21 @@
+var arrayMeans = function() {
+    return {
+        arithmetic: function(arr) {
+            if(!Array.isArray(arr)) {
+                throw new Error("Argument is not an array!");
+            }
+
+            var result = 0;
+
+            arr.forEach(function(e) {
+                result += e;
+            });
+
+            return result / arr.length;
+        }
+    };
+};
+
+if (typeof module !== "undefined" && module.exports) {
+	module.exports = arrayMeans;
+}
