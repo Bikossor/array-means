c0c6dfd194bda9e63ad4fd152ca55fc1cd832cc5
Dergeraetwirdniemuede <44534540+Dergeraetwirdniemuede@users.noreply.github.com>
2018-10-28 00:12:31 +0200

    Fixes #4

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
-var arrayMeans = function() {
-    return {
-        arithmetic: function(arr) {
-            if(!Array.isArray(arr)) {
-                throw new Error("Argument is not an array!");
-            }
-
-            var result = 0;
-
-            arr.forEach(function(e) {
-                result += e;
-            });
-
-            return result / arr.length;
+var arrayMeans = {
+    arithmetic: function (arr) {
+        if (!Array.isArray(arr)) {
+            throw new Error("Argument is not an array!");
         }
-    };
+
+        var result = 0;
+
+        arr.forEach(function (e) {
+            result += e;
+        });
+
+        return result / arr.length;
+    }
 };
 
 if (typeof module !== "undefined" && module.exports) {
-	module.exports = arrayMeans;
+    module.exports = arrayMeans;
 }
