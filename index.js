0e0a93631fa4fd75b5647a27ccbf5f8befd7d353
Andre-PC\Andre <andre.lichtenthaeler@gmail.com>
2018-10-28 02:12:11 +0200

    Closes #11

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
+/**
+ * @name array-means
+ * @version 1.1.0
+ * @description Calculates various averages of an array
+ * @author André Lichtenthäler
+ * @license GPL-3.0
+ */
 var arrayMeans = {
+    /**
+     * @param {Array<Number>} arr - The array of which the arithmetic mean is calculated on
+     * @returns {Number} The arithmetic mean
+     */
     arithmetic: function (arr) {
         if (!Array.isArray(arr)) {
             throw new Error("Argument is not an array!");
@@ -12,6 +23,10 @@ var arrayMeans = {
 
         return result / arr.length;
     },
+    /**
+     * @param {Array<Number>} arr - The array of which the quadratic mean is calculated on
+     * @returns {Number} The quadratic mean
+     */
     quadratic: function(arr) {
         if (!Array.isArray(arr)) {
             throw new Error("Argument is not an array!");
