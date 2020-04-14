7205671825792df8b0068efe83017a9d3a709f72
Bikossor <andre.lichtenthaeler@gmail.com>
2020-04-14 23:43:22 +0200

    Closes #39

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var header = require('gulp-header');
+var pump = require('pump');
+
+var fileName = "array-means";
+var folderDestination = "dist/";
+var package = require('./package.json');
+var headerString = ['/*',
+	'<%= pkg.name %> v<%= pkg.version %> (<%= pkg.license %>) |',
+	'<%= pkg.author %> |',
+	'<%= pkg.homepage %>',
+	'*/\n'
+].join(' ');
+
+gulp.task("build-js", function (callback) {
+	pump([
+		gulp.src("dist/array-means.js"),
+		uglify(),
+		concat(fileName + ".min.js"),
+		header(headerString, {
+			pkg: package
+		}),
+		gulp.dest(folderDestination)
+	], callback);
+});
+
+gulp.task("default",
+	gulp.series("build-js")
+);
