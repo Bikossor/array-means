0eee5e6fd3de740f1254335652c6703500998ce3
Bikossor <andre.lichtenthaeler@gmail.com>
2020-11-16 00:42:14 +0100

    Closes #59

---
+++ dist/esm/arithmetic.d.ts
@@ -0,0 +1,5 @@
+/**
+ * @param {number[]} arr - The array of which the arithmetic mean is calculated on
+ * @returns {number} The arithmetic mean
+ */
+export declare const arithmetic: (arr: number[]) => number;
---
+++ dist/esm/geometric.d.ts
@@ -0,0 +1,5 @@
+/**
+ * @param {number[]} arr - The array of which the geometric mean is calculated on
+ * @returns {number} The geometric mean
+ */
+export declare const geometric: (arr: number[]) => number;
