36b35c35c857b5b5f31a46d0fb6e28e9df346292
Bikossor <andre.lichtenthaeler@gmail.com>
2021-04-24 23:38:44 +0200

    Port the test file to TypeScript
    #70

--- test/array-means.test.ts
+++ test/array-means.test.ts
@@ -1,10 +1,10 @@
-const {
+import {
     arithmetic,
     cubic,
     geometric,
     harmonic,
     quadratic
-} = require('../dist/cjs/index.js');
+} from "../src";
 const dummyArray = [140, 149, 895, 28, 716, 826, 539, 518, 59, 463, 967, 664, 170, 623, 764, 195, 380, 710, 549, 879];
 
 const arithmeticResult = 511.7;
