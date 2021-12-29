ba3c7075ce28861b954e996b15b2e84f04eb755e
André Lichtenthäler <andre.lichtenthaeler@gmail.com>
2021-12-29 18:17:01 +0100

    feat: add type declarations (#79)
    * chore(dev-deps): install merge2 #78
    * build: improve gulpfile to emit dts files #78

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,30 @@
 const gulp = require("gulp");
 const ts = require("gulp-typescript");
 const del = require("del");
+const merge = require("merge2");
 
 const tsProjectCjs = ts.createProject("tsconfig.cjs.json");
 const tsProjectEsm = ts.createProject("tsconfig.esm.json");
+
+const streamCjs = tsProjectCjs.src().pipe(tsProjectCjs());
+const streamEsm = tsProjectEsm.src().pipe(tsProjectEsm());
 
 gulp.task("clean-up", function () {
   return del("dist/**", { force: true });
 });
 
 gulp.task("build-typescript-to-cjs", function () {
-  return tsProjectCjs.src().pipe(tsProjectCjs()).js.pipe(gulp.dest("dist/cjs"));
+  return merge([
+    streamCjs.js.pipe(gulp.dest("dist/cjs")),
+    streamCjs.dts.pipe(gulp.dest("dist/cjs"))
+  ]);
 });
 
 gulp.task("build-typescript-to-esm", function () {
-  return tsProjectEsm.src().pipe(tsProjectEsm()).js.pipe(gulp.dest("dist/esm"));
+  return merge([
+    streamEsm.js.pipe(gulp.dest("dist/esm")),
+    streamEsm.dts.pipe(gulp.dest("dist/esm"))
+  ]);
 });
 
 gulp.task("default", gulp.series("clean-up", "build-typescript-to-cjs", "build-typescript-to-esm"));
