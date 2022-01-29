af6ac19aa2c78a6d9028bf31e54378ad721cd6eb
André Lichtenthäler <andre.lichtenthaeler@gmail.com>
2022-01-29 22:52:37 +0100

    chore: run prettier
    #104

--- test/benchmark.js
+++ test/benchmark.js
@@ -1,37 +1,40 @@
-const dummyArray = [140, 149, 895, 28, 716, 826, 539, 518, 59, 463, 967, 664, 170, 623, 764, 195, 380, 710, 549, 879];
+const dummyArray = [
+  140, 149, 895, 28, 716, 826, 539, 518, 59, 463, 967, 664, 170, 623, 764, 195, 380, 710,
+  549, 879,
+];
 const {
-    arithmetic,
-    cubic,
-    geometric,
-    harmonic,
-    quadratic,
-    median,
-} = require('../dist/cjs/index.js');
-const benchmark = require('benchmark');
-const suite = new benchmark.Suite;
+  arithmetic,
+  cubic,
+  geometric,
+  harmonic,
+  quadratic,
+  median,
+} = require("../dist/cjs/index.js");
+const benchmark = require("benchmark");
+const suite = new benchmark.Suite();
 
 suite
-    .add('arithmetic', function () {
-        arithmetic(dummyArray);
-    })
-    .add('quadratic', function () {
-        quadratic(dummyArray);
-    })
-    .add('harmonic', function () {
-        harmonic(dummyArray);
-    })
-    .add('geometric', function () {
-        geometric(dummyArray);
-    })
-    .add('cubic', function () {
-        cubic(dummyArray);
-    })
-    .add('median', function () {
-        median(dummyArray);
-    })
-    .on('cycle', function (event) {
-        console.log(String(event.target));
-    })
-    .run({
-        'async': true
-    });
+  .add("arithmetic", function () {
+    arithmetic(dummyArray);
+  })
+  .add("quadratic", function () {
+    quadratic(dummyArray);
+  })
+  .add("harmonic", function () {
+    harmonic(dummyArray);
+  })
+  .add("geometric", function () {
+    geometric(dummyArray);
+  })
+  .add("cubic", function () {
+    cubic(dummyArray);
+  })
+  .add("median", function () {
+    median(dummyArray);
+  })
+  .on("cycle", function (event) {
+    console.log(String(event.target));
+  })
+  .run({
+    async: true,
+  });
