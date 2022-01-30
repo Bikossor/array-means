6cfb1db42997a4513ab1751793d9093019b4f4f2
André Lichtenthäler <andre.lichtenthaeler@gmail.com>
2022-01-30 02:53:10 +0100

    test: switch to `ts-jest` (#109)
    * chore: remove babel config file
    
    #108
    
    * deps: remove babel, install ts-jest and update dependencies
    
    #108
    
    * test: set jest config to utilize ts-jest
    
    #108

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,8 @@ import type { Config } from "@jest/types";
 const config: Config.InitialOptions = {
   verbose: true,
   modulePathIgnorePatterns: ["<rootDir>/dist/"],
+  preset: 'ts-jest',
+  testEnvironment: 'node',
 };
 
 export default config;
