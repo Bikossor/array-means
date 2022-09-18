7e5795cfb59d7d83e827316359443141cae4bff3
Bikossor <andre.lichtenthaeler@gmail.com>
2022-09-18 15:00:33 +0200

    test: improve `median` tests
    #169

--- src/median.test.ts
+++ src/median.test.ts
@@ -1,14 +1,27 @@
 import { median } from "./median";
 
-const dummyArray = [
-  140, 149, 895, 28, 716, 826, 539, 518, 59, 463, 967, 664, 170, 623, 764, 195, 380, 710,
-  549, 879,
-];
-
-const expectedResult = 544;
-
 describe("Median mean", () => {
-  it("should return the expected result if the specified parameter is valid", () => {
+  it("should return the expected result if the specified parameter is valid and has an even length", () => {
+    // even amount of elements (20)
+    const dummyArray = [
+      140, 149, 895, 28, 716, 826, 539, 518, 59, 463, 967, 664, 170, 623, 764, 195, 380,
+      710, 549, 879,
+    ];
+
+    const expectedResult = 544;
+
+    expect(median(dummyArray)).toBe(expectedResult);
+  });
+
+  it("should return the expected result if the specified parameter is valid and has an odd length", () => {
+    // odd amount of elements (21)
+    const dummyArray = [
+      140, 149, 895, 28, 716, 826, 539, 518, 59, 463, 967, 664, 170, 623, 764, 195, 380,
+      710, 549, 879, 33
+    ];
+
+    const expectedResult = 539;
+
     expect(median(dummyArray)).toBe(expectedResult);
   });
 
